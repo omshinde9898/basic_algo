98d12bc415655071d8402461d6d0b458836df089
OmShinde <omshinde9898@gmail.com>
2022-10-14 20:53:12 +0530

    Documentation Updates

--- binary_search.c
+++ binary_search.c
@@ -1,17 +1,32 @@
+// Binary search algorithm for faster searching through array 
+// Condition : Array should be sorted then we can apply this search
+
+
 int binary_search(int a[],int i,int j,int x){
+    /*
+        binary_search(a[] ,int i ,int j ,int x )
+        a[] : one dimensional array (sorted)
+        i : starting index of array
+        j : last index of array
+        x : target value
+
+    Definition : Binary search is algorithm used to search target throgh 1-D sorted array which have 
+                    logarithmic time complexity hence efficient than linear search 
+    */
     
-    int c;
+    int c;      // Division variable
     
+    // Initial check
     if(i>j){
         return(-1);
     }
-    
+    // Division of array into two parts 
     c = (i+j)/2;
-    
+    // Comparision of current element with target
     if(x==a[c]){
         return(c);
     }
-    
+    // Use of reccursion to continue proceess to last element
     if(x>a[c]){
         return(binary_search(a,c+1,j,x));
     }else{
